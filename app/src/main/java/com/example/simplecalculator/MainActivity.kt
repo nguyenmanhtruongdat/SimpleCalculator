28618d3654e989cb9713033d3d957192cc873635
nguyenmanhtruongdat <datnmt.21it@vku.udn.vn>
2023-02-16 01:12:22 +0700

    dat

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -6,168 +6,174 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.simplecalculator.databinding.ActivityMainBinding
-
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var canAddOperation = false
-    private var canAddDecimal = true
-
-
+    private lateinit var expression: Expression
+    private var lastNumeric = false
+    private var stateError = false
+    private var lastDot = false
+    private var isSinClicked= false
+    private var isCosClicked= false
+    private var isTanClicked= false
+    private var isCotClicked= false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
     }
 
-
-    fun numberAction(view: View) {
-        if (view is Button) {
-            if (view.text == ".") {
-                if (canAddDecimal) {
-                    binding.workingsTV.append(view.text)
+    fun onDigitClick(view: View) {
+        if (stateError) {
+            binding.workingsTV.text = (view as Button).text
+            stateError = false
+        } else {
+            val decimalButton = (view as Button).text
+            if (decimalButton == ".") {
+                if (!binding.workingsTV.text.contains(".")) {
+                    binding.workingsTV.append(decimalButton)
                 }
-                canAddDecimal = false
-            } else
-
-                binding.workingsTV.append(view.text)
-
-                canAddOperation = true
+            } else {
+                binding.workingsTV.append(decimalButton)
+            }
         }
+        lastNumeric = true
     }
 
 
-    fun operationAction(view: View) {
-        if (view is Button && canAddOperation) {
-            binding.workingsTV.append(view.text)
-            canAddOperation = false
-            canAddDecimal = true
-        }
-    }
-
-    fun backSpaceAction(view: View) {
-        val length = binding.workingsTV.length()
-        if (length > 0)
-            binding.workingsTV.text = binding.workingsTV.text.subSequence(0, length - 1)
-    }
-
-    fun allClearAction(view: View) {
+    fun onAllClearClick(view: View) {
         binding.workingsTV.text = ""
         binding.resultsTV.text = ""
-        canAddOperation = false
+        stateError = false
+        lastDot = false
+        lastNumeric = false
+
     }
 
+    fun onEqualClick(view: View) {
+        onEqual()
+        binding.workingsTV.text = ""
 
-    fun equalsAction(view: View) {
-        binding.resultsTV.text = calculateResults()
     }
 
-    private fun calculateResults(): String {
-        val digitsOperators = digitsOperators()
-        if (digitsOperators.isEmpty()) return ""
+    fun onOperatorClick(view: View) {
+        if (!stateError && lastNumeric) {
+            binding.workingsTV.append((view as Button).text)
+            lastDot = false
+            lastNumeric = false
+            onEqual()
 
-        val timesDivision = timesDivisionCalculate(digitsOperators)
-        if (timesDivision.isEmpty()) return ""
-
-        val result = addSubtractCalculate(timesDivision)
-        return result.toString()
+        }
     }
 
+//    fun onOperatorClick(view: View) {
+//        if (!stateError && lastNumeric) {
+//            val currentOperator = (view as Button).text.toString()
+//            val workings = binding.workingsTV.text.toString()
+//
+//            if (workings.isNotEmpty()) {
+//                val lastChar = workings[workings.length - 1]
+//                if (lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/') {
+//                    // replace the last operator with the new one
+//                    val newWorkings = workings.substring(0, workings.length - 1) + currentOperator
+//                    binding.workingsTV.text = newWorkings
+//                } else {
+//                    // append the new operator
+//                    binding.workingsTV.append(currentOperator)
+//                }
+//            } else {
+//                // the first character cannot be an operator
+//                binding.workingsTV.append("0$currentOperator")
+//            }
+//
+//            lastDot = false
+//            lastNumeric = false
+//        }
+//    }
 
-    private fun addSubtractCalculate(passedList: MutableList<Any>): Float {
-        var result = passedList[0] as Float
 
-        for (i in passedList.indices) {
-            if (passedList[i] is Char && i != passedList.lastIndex) {
-                val operator = passedList[i]
-                val nextDigit = passedList[i + 1] as Float
-                if (operator == '+')
-                    result += nextDigit
-                if (operator == '-')
-                    result -= nextDigit
+
+    fun backSpaceClick(view: View) {
+        binding.workingsTV.text = binding.workingsTV.text.toString().dropLast(1)
+        try {
+            var lastNum = binding.workingsTV.text.toString().last()
+//            if (lastNum.isDigit()){
+//                onEqual()
+//            }
+        } catch (e: java.lang.Exception) {
+            binding.resultsTV.text = ""
+
+        }
+    }
+
+//    private fun onEqual() {
+//        if (lastNumeric && !stateError) {
+//            val data = binding.workingsTV.text.toString()
+//            expression = ExpressionBuilder(data).build()
+//            try {
+//                val result = expression.evaluate()
+//                binding.resultsTV.text = result.toString()
+//
+//            } catch (ex: java.lang.ArithmeticException) {
+//                Log.e("evaluate exception", ex.toString())
+//                binding.resultsTV.text = "Error"
+//                stateError = true
+//                lastNumeric = false
+//            }
+//        }
+//    }
+
+    private fun onEqual() {
+
+        if (lastNumeric && !stateError) {
+            var data = binding.workingsTV.text.toString()
+
+            if (isSinClicked) {
+                // If sin button was clicked, wrap the expression in sin()
+                data = "sin($data)"
+                isSinClicked = false
+                data = data.replace("sin(", "radians(sin(")
+            }
+            if (isCosClicked) {
+                // If sin button was clicked, wrap the expression in sin()
+                data = "cos($data)"
+                isSinClicked = false
+                data = data.replace("cos(", "radians(cos(")
+            }
+            if (isTanClicked) {
+                // If sin button was clicked, wrap the expression in sin()
+                data = "tan($data)"
+                isSinClicked = false
+                data = data.replace("tan(", "radians(tan(")
+            }
+            if (isCotClicked) {
+                // If sin button was clicked, wrap the expression in sin()
+                data = "cot($data)"
+                isSinClicked = false
+                data = data.replace("cot(", "radians(cot(")
+            }
+
+            val expression = ExpressionBuilder(data).build()
+
+            try {
+                val result = expression.evaluate()
+                binding.resultsTV.text = result.toString()
+            } catch (ex: ArithmeticException) {
+                Log.e("evaluate exception", ex.toString())
+                binding.resultsTV.text = "Error"
+                stateError = true
+                lastNumeric = false
             }
         }
-
-        return result
     }
 
 
-    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
-        var list = passedList
-        while (list.contains('×') || list.contains('÷')) {
-            list = calcTimesDiv(list)
-        }
-        return list
-    }
 
-
-    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
-        val newList = mutableListOf<Any>()
-        var restartIndex = passedList.size
-
-        for (i in passedList.indices) {
-            if (passedList[i] is Char && i != passedList.lastIndex && i < restartIndex) {
-                val operator = passedList[i]
-                val prevDigit = passedList[i - 1] as Float
-                val nextDigit = passedList[i + 1] as Float
-                when (operator) {
-                    '×' -> {
-                        newList.add(prevDigit * nextDigit)
-                        restartIndex = i + 1
-                    }
-                    '÷' -> {
-                        newList.add(prevDigit / nextDigit)
-                        restartIndex = i + 1
-                    }
-//                    '%' -> {
-//                        newList.add(prevDigit / 100)
-//                        restartIndex = i + 1
-//                    }
-                    else -> {
-                        newList.add(prevDigit)
-                        newList.add(operator)
-                    }
-                }
-            }
-
-            if (i > restartIndex)
-                newList.add(passedList[i])
-        }
-
-        return newList
-    }
-
-
-    private fun digitsOperators(): MutableList<Any> {
-        val list = mutableListOf<Any>()
-        var currentDigit = ""
-        for (character in binding.workingsTV.text) {
-            if (character.isDigit() || character == '.')
-                currentDigit += character
-            else {
-                list.add(currentDigit.toFloat())
-                currentDigit = ""
-                list.add(character)
-            }
-        }
-        if (currentDigit != "")
-            list.add(currentDigit.toFloat())
-        return list
-    }
 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
